3076fde369bf9274c53e527ce6c2ce981c23c51f
ifotn <Rich.Freeman@GeorgianCollege.ca>
2025-01-20 17:28:35 -0500

    l3/2 - GET 1, POST

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 // controllers
 import cheesesController from './controllers/cheeses.js';
@@ -6,8 +7,10 @@ import cheesesController from './controllers/cheeses.js';
 // create express server object
 const app = express();
 
+app.use(bodyParser.json());
+
 // url dispatching
-app.use('/cheeses', cheesesController);
+app.use('/api/v1/cheeses', cheesesController);
 
 // start web server
 app.listen(3000, () => {
