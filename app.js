4a26f432e617740697bdd7491815a4884960f935
ifotn <Rich.Freeman@GeorgianCollege.ca>
2025-03-17 16:33:57 -0400

    l10/2 - jwt config

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ import path from 'path';
 import passport from 'passport';
 import User from './models/user.js';
 
+// jwt auth
+import cookieParser from 'cookie-parser';
+import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
+
 // controllers
 import cheesesController from './controllers/cheeses.js';
 import usersController from './controllers/users.js';
@@ -20,6 +24,7 @@ import usersController from './controllers/users.js';
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 // get public path for angular client app
 const __dirname = path.resolve();
@@ -56,6 +61,30 @@ app.use(passport.initialize());
 
 // passport-local is the default
 passport.use(User.createStrategy());
+
+// jwt config
+const jwtOptions = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.PASSPORT_SECRET
+};
+
+// jwt strategy
+let strategy = new JwtStrategy(jwtOptions, async (jwt_payload, callback) => {
+    try {
+        const user = await User.findById(jwt_payload.id);
+        if (user) {
+            // user exists, send back no error plus the user object
+            return callback(null, user);
+        }
+        // user not found
+        return callback(null, false);
+    }
+    catch (err) {
+        return callback(err, false);
+    }
+});
+
+passport.use(strategy);
 
 // url dispatching
 app.use('/api/v1/cheeses', cheesesController);
--- controllers/users.js
+++ controllers/users.js
@@ -9,10 +9,32 @@ router.post('/register', async (req, res) => {
     // use passport-local-mongoose inherited methods in user model to try creating new user
     // create user first, then add pw second so it gets salted & hashed
     try {
-        const user = new User({ username: req.body.username });
+        // duplicate username check
+        let user = await User.findOne({ username: req.body.username });
+
+        if (user) {
+            return res.status(400).json({ msg: 'User already exists' });
+        }
+        
+        user = new User({ username: req.body.username });
         await user.setPassword(req.body.password);
         await user.save();
-        return res.status(201).json(user);
+        return res.status(201).json({ msg: 'User registered successfully' });
+    }
+    catch (err) {
+        return res.status(400).json(err);
+    }
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        const { user } = await User.authenticate()(req.body.username, req.body.password);
+        if (user) {
+            return res.status(200).json(req.body.username);
+        }
+        else {
+            return res.status(401).json({ msg: 'Invalid Login' });
+        }
     }
     catch (err) {
         return res.status(400).json(err);
